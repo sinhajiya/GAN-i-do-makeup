95afadb341f709df71adbc67a6e501e09c2905bb
Jiya Sinha <130081533+sinhajiya@users.noreply.github.com>
2025-08-02 19:21:10 +0530

    changed vgg loss

--- losses.py
+++ losses.py
@@ -96,8 +96,6 @@ class VGG19_feature(nn.Module):
             vgg_pretrained[12:21],# r41
             vgg_pretrained[21:30] # r51
         ])
-        self.mean = torch.Tensor([0.485, 0.456, 0.406]).view(1,3,1,1)
-        self.std = torch.Tensor([0.229, 0.224, 0.225]).view(1,3,1,1)
         self.register_buffer('mean', torch.Tensor([0.485, 0.456, 0.406]).view(1,3,1,1))
         self.register_buffer('std', torch.Tensor([0.229, 0.224, 0.225]).view(1,3,1,1))
 
